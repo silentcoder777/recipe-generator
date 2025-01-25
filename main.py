689ab1502978f8d12b79d5437363af1aa7313328
silentcoder777 <xyz.gmail.com>
2025-01-25 14:22:49 -0600

    Adding new config file which will include all configs and secrets

--- main.py
+++ main.py
@@ -4,21 +4,26 @@
 
 from openai import OpenAI
 import requests
-from config import OPENAI_API_KEY
+import json
 
+#Read config file
+# Open the config.json file
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
 ###############
 # Variables
 ##############
 chatModel = "gpt-4o-mini"
 testChatMessage = "Hello. who are you?"
+openAIKey = config['OPENAI_API_KEY']
 
 # URL
 chatURL = "https://api.openai.com/v1/chat/completions"
 
 # Headers
 chatHeader = {
-    'Authorization': f'Bearer {OPENAI_API_KEY}',
+    'Authorization': f'Bearer {openAIKey}',
     'Content-Type': 'application/json'
 }
 
