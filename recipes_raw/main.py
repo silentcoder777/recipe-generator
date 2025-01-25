146571a5655214651cdedd395f93c8a7da8a3022
silentcoder777 <xyz.gmail.com>
2025-01-25 14:09:34 -0600

    Adding client, adding a config file to get the key

--- recipes_raw/main.py
+++ recipes_raw/main.py
@@ -1,7 +1,43 @@
-import pandas as pd
-from tabulate import tabulate
+########################
+# Library imports
+########################
+
+from openai import OpenAI
+import requests
+from config import OPENAI_API_KEY
 
 
-dataset = pd.read_json("C:\\My Files\\Personal Projects\Recipe Generator\\recipes_raw\\recipes_raw_nosource_ar.json")
-df = pd.DataFrame(dataset.head())
-print(tabulate(df, headers='keys', tablefmt='psql'))
+###############
+# Variables
+##############
+chatModel = "gpt-4o-mini"
+testChatMessage = "Hello. who are you?"
+
+# URL
+chatURL = "https://api.openai.com/v1/chat/completions"
+
+# Headers
+chatHeader = {
+    'Authorization': f'Bearer {OPENAI_API_KEY}',
+    'Content-Type': 'application/json'
+}
+
+
+chatData = {
+    'model': chatModel,
+    'messages':  [
+        {
+            "role": "user",
+            "content": testChatMessage
+        }
+    ]
+}
+
+response = requests.post(chatURL, json = chatData, headers = chatHeader)
+
+
+if response.status_code == 200:
+    print("Request was successful!")
+    print(response.json())
+else:
+    print("Failed to send the request. Status code:", response.status_code, response.json())
