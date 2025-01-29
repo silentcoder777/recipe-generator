31f17778567735b727d8fe8e46a73ce1bdedb3bd
silentcoder777 <xyz.gmail.com>
2025-01-28 21:27:32 -0600

    adding a new react component

---
+++ find-recipe/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import { WelcomeText } from './landingPage';
+import "./welcomeMessage.css"
+
+function App() {
+  return (
+      <WelcomeText/>
+  );
+}
+
+export default App;
---
+++ find-recipe/src/landingPage.js
@@ -0,0 +1,8 @@
+export function WelcomeText() {
+    return (
+        <div className="welcome-container">
+            <h1 className="welcome-text">Welcome to my Recipe Generator</h1>
+        </div>
+    );
+  }
+
